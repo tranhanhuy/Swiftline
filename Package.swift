61d7f2093729307b58c8b5a00e1c41846350902a
tran.han.huy <tran.han.huy@sun-asterisk.com>
2020-08-18 15:39:39 +0700

    Updated product

--- Package.swift
+++ Package.swift
@@ -2,5 +2,11 @@
 import PackageDescription
 
 let package = Package(
-  name: "Swiftline"
+  name: "Swiftline",
+products: [
+       // Products define the executables and libraries produced by a package, and make them visible to other packages.
+       .library(
+           name: "Swiftline",
+           targets: ["Swiftline"])
+   ],
 )
