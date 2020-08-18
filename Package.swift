fc1fcbed05992ff46dccc9d5d4b2c006d9b9ab1c
tran.han.huy <tran.han.huy@sun-asterisk.com>
2020-08-18 15:41:06 +0700

    Fixed comment

--- Package.swift
+++ Package.swift
@@ -3,10 +3,10 @@ import PackageDescription
 
 let package = Package(
   name: "Swiftline",
-products: [
+  products: [
        // Products define the executables and libraries produced by a package, and make them visible to other packages.
        .library(
            name: "Swiftline",
            targets: ["Swiftline"])
-   ],
+   ]
 )
